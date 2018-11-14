842cf80fc7cba8b67b77d79cf3202e92de40ba34
YVONNEANYANGO <ivonnahbonswuh@gmail.com>
2018-11-14 19:23:23 +0300

    start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export API_KEY='3a2b40d2ab5d476b83912d278645a838'
-export SECRET_KEY='0x20e4bd8zfghfgvcsvubycugtwyfscguihsuigu'
+export SECRET_KEY='0x20e4bd8zfghfgvcsvubysfdghjcugtwyfscguihsuigu'
 
 
 python3.6 manage.py server
